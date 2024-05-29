12e4d85cc3982181c92901234e90ad07e2cd6a24
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-29 07:37:00 +0200

    Implement Flutter on Web and Android - working first time in example counter new

---
+++ counter_new/README.md
@@ -0,0 +1,53 @@
+# counter_new
+
+A flutter sample app that connects to local canister
+
+## Quick Start
+1. `dfx` installed to your local machine, making sure you can deploy canister to your local machine.
+2. `flutter` installed, and use `flutter doctor -v` make sure everything works
+3. `flutter pub get` before running this example.
+4. If you want to use MacOS to debug, make sure `macos/Runner/DebugProfile.entitlements` and `macos/Runner/Release.entitlements` have content below
+    ```xml
+    <dict>
+        ...
+        <key>com.apple.security.network.client</key>
+        <true/>
+    </dict>
+    ```
+5. Deploy canister use `dfx deploy`, the `counter` canister will be running on your local replica. Make sure you put the counter canister id inside `.dfx/local/canister_ids.json` to your `main.dart`, the json like this:
+    ```json
+    {
+        "__Candid_UI": {
+            "local": "x2dwq-7aaaa-aaaaa-aaaxq-cai"
+        },
+        "counter": {
+            "local": "x5cqe-syaaa-aaaaa-aaaxa-cai"
+        }
+    }
+    ```
+6. inside `main.dart`, you should change settings with `canisterId` to your actual id.
+   ```dart
+    Future<void> initCounter({Identity? identity}) async {
+        
+        // initialize counter, change canister id here
+        counter = Counter(canisterId: 'x5cqe-syaaa-aaaaa-aaaxa-cai', url: 'http://localhost:8000');
+        // set agent when other paramater comes in like new Identity
+        await counter?.setAgent(newIdentity: identity);
+        
+    }
+   ```
+7. start
+    ```bash
+    flutter run
+    ```
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ counter_new/lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:agent_dart/agent_dart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+// import Counter class with canister call
+import 'counter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _loading = false;
+  // setup state class variable;
+  Counter? counter;
+
+  @override
+  void initState() {
+    initCounter();
+    super.initState();
+  }
+
+  Future<void> initCounter({Identity? identity}) async {
+    // initialize counter, change canister id here 
+     //10.0.2.2  ? private const val BASE_URL = "http://10.0.2.2:4944"
+
+    String url;
+    if (kIsWeb) {
+      url = 'http://localhost:4944'; 
+    } else {
+      url = 'http://10.0.2.2:4944'; // default to localhost for other platforms
+
+    }
+
+    counter = Counter(canisterId: 'bkyz2-fmaaa-aaaaa-qaaaq-cai', url: url);    // set agent when other paramater comes in like new Identity
+    await counter?.setAgent(newIdentity: identity);
+    await getValue();
+  }
+
+  // get value from canister
+  Future<void> getValue() async {
+    var counterValue = await counter?.getValue();
+    setState(() {
+      _counter = counterValue ?? _counter;
+      _loading = false;
+    });
+  }
+
+  // increment counter
+  Future<void> _incrementCounter() async {
+    setState(() {
+      _loading = true;
+    });
+    await counter?.increment();
+    await getValue();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'The canister counter is now:',
+            ),
+            Text(
+              _loading ? 'loading...' : '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
